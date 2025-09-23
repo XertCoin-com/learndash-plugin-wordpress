f890936dbaa7a4aab6d9f68d79468e1d0f755288
Pexelle Developer <160237941+pexelledeveloper@users.noreply.github.com>
2025-09-24 11:04:12 +1200

    Update pexelle-share-learndash.php

--- pexelle-share-learndash/pexelle-share-learndash.php
+++ pexelle-share-learndash/pexelle-share-learndash.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Pexelle Share for LearnDash
  * Description: Adds a "Share to Pexelle" button next to LearnDash certificate icon, showing a QR modal of the certificate URL.
- * Version: 1.0.4
+ * Version: 1.0.8
  * Author: Pexelle
  * Requires at least: 6.0
  * Requires PHP: 7.4
@@ -20,6 +20,7 @@ require_once PSL_PLUGIN_DIR . 'inc/class-psl-plugin.php';
 require_once PSL_PLUGIN_DIR . 'inc/class-psl-admin.php';
 require_once PSL_PLUGIN_DIR . 'inc/class-psl-frontend.php';
 require_once PSL_PLUGIN_DIR . 'inc/class-psl-magic-login.php';
+require_once PSL_PLUGIN_DIR . 'inc/class-psl-export.php';
 
 add_action('plugins_loaded', static function () {
     if (!class_exists('SFWD_LMS')) {
@@ -33,4 +34,5 @@ add_action('plugins_loaded', static function () {
     \PSL\Psl_Admin::init();
     \PSL\Psl_Frontend::init();
     \PSL\Psl_Magic_Login::init();
+    \PSL\Psl_Export::init();
 });
