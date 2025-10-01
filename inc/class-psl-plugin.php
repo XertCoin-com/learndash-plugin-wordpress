fda9f3cb8a249515e20879d1371cc8eb7344b1c1
Pexelle Developer <160237941+pexelledeveloper@users.noreply.github.com>
2025-10-02 11:39:25 +1300

    Update class-psl-plugin.php

--- inc/class-psl-plugin.php
+++ inc/class-psl-plugin.php
@@ -13,10 +13,6 @@ final class Psl_Plugin {
         add_action('init', [__CLASS__, 'load_textdomain']);
     }
 
-    public static function load_textdomain() {
-        load_plugin_textdomain('psl', false, dirname(plugin_basename(PSL_PLUGIN_FILE)) . '/languages/');
-    }
-
     public static function maybe_set_defaults() {
         $opts = get_option(self::OPTION_KEY);
         if (!$opts || !is_array($opts)) {
